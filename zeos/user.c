b0ff98ffe15d30e007cb3bf63764076ff748a8b6
Navitk <120760085+NaviTK@users.noreply.github.com>
2026-03-05 17:02:40 +0100

    Comment out test write statements in user.c

--- zeos/user.c
+++ zeos/user.c
@@ -21,6 +21,10 @@ int __attribute__ ((__section__(".text.main")))
   	//if (write(1, "El siguiente write dara error de canal:\n", 40) < 0) perror();
   	//if (write(2, "Hello World!\n", 13) < 0) perror();
   	//if (write(1, "FUNCIONA!\n", 10) < 0) perror();
+	//Test Write
+	//if (fast_write(1, "Tambien pasara lo mismo con fast_write:\n", 40) < 0) perror();
+  	//if (fast_write(2, "Hello World!\n", 13) < 0) perror();
+  	if (fast_write(1, "FUNCIONA!\n", 10) < 0) perror();
 	
  	// para testear el pagefault ->
  	//char* p = 0;
