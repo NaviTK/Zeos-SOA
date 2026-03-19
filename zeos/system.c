f57385f79fc5445880ed8b53a24a8059d72502f0
ByZeleon <r.g.g.20049@gmail.com>
2026-03-19 18:18:50 +0100

    init_task hecho, task_switch a medias

--- zeos/system.c
+++ zeos/system.c
@@ -66,7 +66,7 @@ int __attribute__((__section__(".text.main")))
 
 
   printk("Entering user mode...");
-  //set_pe_flag();   /* Cuando se quite el prepare_mono_address_space */
+  //set_pe_flag();   /*Cuando se quite el prepare_mono_address_space */
   enable_int();
   /*
    * We return from a 'theorical' call to a 'call gate' to reduce our privileges
